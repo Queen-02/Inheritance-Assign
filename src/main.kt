ea76986016257221c5247989d23f9fab2aa2d540
Queen-02 <queenzyblack176@gmail.com>
2022-04-01 19:14:16 +0300

    added codes to main.kt

--- src/main.kt
+++ src/main.kt
@@ -1,32 +1,25 @@
 fun main() {
     var harrier = Car("Toyota", " Compact crossover SUV ", "White", 5)
+    var scania = Bus("Isuzu", "volvo", "Blue and black", 60)
+
+    //class car
+    harrier.carry(5)
+    harrier.identity()
+    var fees = harrier.calculatePakingFees(5)
+    println(fees)
+
+    //class bus
+    scania.carry(65)
+    scania.identity()
+    var totalFees=harrier.calculatePakingFees(10)
+    println(totalFees)
 
 }
 
 //create a class called car
-class Car(var make: String, var model: String, var color: String, var capacity: Int) {
+open class Car(var make: String, var model: String, var color: String, var capacity: Int) {
     fun carry(people: Int) {
         if (people == capacity) {
-            println("Carrying $people passengers")
-        } else {
-            var x = capacity + 2
-            println("Over capacity by $x people")
-        }
-    }
-
-    fun identity() {
-        println("I am a $color $make $model")
-    }
-
-    fun calculatePakingFees(hours: Int): Int {
-        var parkingFees = hours * 20
-        return parkingFees
-    }
-}
-
-class Bus(var make: String, var model: String, var color: String, var capacity: Int) {
-    fun carry(people: Int) {
-        if (people == 5) {
             println("Carrying $people passengers")
         } else {
             var x = 2
@@ -38,14 +31,24 @@ class Bus(var make: String, var model: String, var color: String, var capacity: 
         println("I am a $color $make $model")
     }
 
-    fun calculatePakingFees(hours: Int): Int {
+    open fun calculatePakingFees(hours: Int): Int {
         var parkingFees = hours * 20
         return parkingFees
+    }
+}
+
+class Bus(make: String, model: String, color: String, capacity: Int):Car(make, model, color, capacity) {
+
+    override fun calculatePakingFees(hours: Int): Int {
+        return super.calculatePakingFees(hours)
+        return capacity
     }
 
     fun maxTripFare(fare: Double): Double {
         var maxFare = fare * capacity
         return maxFare
     }
+
+
 }
 
